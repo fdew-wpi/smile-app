8101b0bfdb43b24ef701bbf472ff703a516445ae
Frankiepy <francis.dewander@jdjdesign.com>
2025-11-07 10:43:36 -0500

    Implement Milestone 2 features: tags and sorting

--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,4 @@
+
 from datetime import datetime, timezone
 from typing import Optional
 
@@ -5,10 +6,32 @@ from app import db
 import sqlalchemy as sqla
 import sqlalchemy.orm as sqlo
 
+postTags = db.Table(
+    'post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
+class Tag(db.Model):
+    id: sqlo.Mapped[int] = sqlo.mapped_column(primary_key=True)
+    name: sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(20))
+    posts: sqlo.WriteOnlyMapped['Post'] = sqlo.relationship(
+        back_populates='tags', secondary=postTags
+    )
+
+    def __repr__(self):
+        return f'<Tag {self.id}: {self.name}>'
+
 class Post(db.Model):
     id : sqlo.Mapped[int] = sqlo.mapped_column(primary_key=True)
     title : sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(150))
     body: sqlo.Mapped[str] = sqlo.mapped_column(sqla.String(1500))
     timestamp : sqlo.Mapped[Optional[datetime]] = sqlo.mapped_column(default = lambda : datetime.now(timezone.utc)) 
-    likes: sqlo.Mapped[int] = sqlo.mapped_column(sqla.Integer, default=0)
+    like_count: sqlo.Mapped[int] = sqlo.mapped_column(sqla.Integer, default=0)
     happiness_level : sqlo.Mapped[int] = sqlo.mapped_column(sqla.Integer, default = 3)
+    tags: sqlo.WriteOnlyMapped['Tag'] = sqlo.relationship(
+        back_populates='posts', secondary=postTags
+    )
+
+    def get_tags(self):
+        return db.session.scalars(self.tags.select()).all()
--- smile.py
+++ smile.py
@@ -1,17 +1,25 @@
+
 from config import Config
 
 from app import create_app, db
-from app.main.models import Post
+from app.main.models import Post, Tag
 import sqlalchemy as sqla
 import sqlalchemy.orm as sqlo
+from sqlalchemy import event
 
 app = create_app(Config)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'sqla': sqla, 'sqlo': sqlo, 'db': db, 'Post': Post }
+    return {'sqla': sqla, 'sqlo': sqlo, 'db': db, 'Post': Post, 'Tag': Tag }
 
 tags = ['funny','inspiring', 'true-story', 'heartwarming', 'friendship']
+
+@event.listens_for(Tag.__table__, 'after_create')
+def insert_tags(*args, **kwargs):
+    for t in tags:
+        db.session.add(Tag(name=t))
+    db.session.commit()
 
 @app.before_request
 def initDB(*args, **kwargs):
